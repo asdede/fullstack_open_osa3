e0c71528ac78638488b85b0479ad782d41970474
asdasdasd <alexitikkala@kamk.fi>
2023-06-27 14:19:56 +0300

    build

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const cors = require('cors')
 
 app.use(express.json())
 app.use(cors())
-
+app.use(express.static('build'))
 
 morgan.token("requestData",(req) => {
     return JSON.stringify(req.body);
