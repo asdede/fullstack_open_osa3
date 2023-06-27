de5f276442852a338b355c8d036c35193001f8d7
asdasdasd <alexitikkala@kamk.fi>
2023-06-27 19:19:44 +0300

    ehkä nyt

--- puhelinluettelo/src/index.js
+++ puhelinluettelo/src/index.js
@@ -5,7 +5,5 @@ import "./index.css"
 
 
 
-axios.get('http://localhost:3001/api/persons').then(response => {
-  const notes = response.data
-  ReactDOM.createRoot(document.getElementById('root')).render(<App notes={notes} />)
-});
+
+  ReactDOM.createRoot(document.getElementById('root')).render(<App />)
