e7cd9ef7ad1ca43abd970715ebb91f9cdedb070c
asdasdasd <alexitikkala@kamk.fi>
2023-06-27 15:06:28 +0300

    korjaus

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const app = express()
 // const cors = require('cors')
 
 app.use(express.json())
-app.use(cors())
+// app.use(cors())
 app.use(express.static('build'))
 
 morgan.token("requestData",(req) => {
